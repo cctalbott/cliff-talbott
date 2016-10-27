531053986692407f51ea23529e6bf756751ac95a
Clint Talbott <cctalbott@gmail.com>
2016-10-27 10:15:06 -0500

    can now switch audio track being played. Next incorporate article for each track on page.

--- app/cliff-talbott/cliff-talbott.component.js
+++ app/cliff-talbott/cliff-talbott.component.js
@@ -12,18 +12,25 @@ angular.
     templateUrl: 'cliff-talbott/cliff-talbott.template.html',
     controller: function($sce, $http) {
       var self = this;
+      self.state = null;
+      self.API = null;
+      
       $http.get("interviews/interviews.json").then(function(response) {
         self.interviews = response.data;
         self.activeInterview = self.interviews[0];
         self.config = {
-          sources: [
-            {
-              src: $sce.trustAsResourceUrl(self.activeInterview.source), 
-              type: "audio/mpeg"
-            }
-          ],
+          sources: self.activeInterview.sources,
           theme: {url: "styles/videogular.css"}
         };
       });
+      
+      self.onPlayerReady = function(API) {
+        self.API = API;
+      };
+      self.setAudio = function(id) {
+        self.API.stop();
+        self.activeInterview = self.interviews[id];
+        self.config.sources = self.activeInterview.sources;
+      };
     }
   });
