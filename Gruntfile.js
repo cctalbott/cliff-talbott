76298f919e0b5f968527978d14d871e7ccd61b57
Clint Talbott <cctalbott@gmail.com>
2016-11-01 14:18:53 -0500

    grunt working pretty good now going to merge into master

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,65 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      app_bower_components: {
+        files: {
+          './app/min-safe/bower_components/angular/angular.js': ['./app/bower_components/angular/angular.js'],
+          './app/min-safe/bower_components/angular-route/angular-route.js': ['./app/bower_components/angular-route/angular-route.js'],
+          './app/min-safe/bower_components/angular-sanitize/angular-sanitize.js': ['./app/bower_components/angular-sanitize/angular-sanitize.js'],
+          './app/min-safe/bower_components/videogular/videogular.js': ['./app/bower_components/videogular/videogular.js'],
+          './app/min-safe/bower_components/videogular-controls/vg-controls.js': ['./app/bower_components/videogular-controls/vg-controls.js'],
+          './app/min-safe/bower_components/videogular-buffering/vg-buffering.js': ['./app/bower_components/videogular-buffering/vg-buffering.js']
+        }
+      },
+      app: {
+        files: {
+          './app/min-safe/app.module.js': ['./app/app.module.js'],
+          './app/min-safe/cliff-talbott/cliff-talbott.module.js': ['./app/cliff-talbott/cliff-talbott.module.js'],
+          './app/min-safe/cliff-talbott/cliff-talbott.component.js': ['./app/cliff-talbott/cliff-talbott.component.js']
+        }
+      }
+    },
+    concat: {
+      bower_components: {
+        options: {
+          // Replace all 'use strict' statements in the code with a single one at the top
+          banner: "'use strict';\n",
+          process: function(src, filepath) {
+            return '// Source: ' + filepath + '\n' +
+              src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+          },
+        },
+        src: [
+          './app/min-safe/bower_components/angular/angular.js', 
+          './app/min-safe/bower_components/angular-route/angular-route.js',
+          './app/min-safe/bower_components/angular-sanitize/angular-sanitize.js',
+          './app/min-safe/bower_components/videogular/videogular.js',
+          './app/min-safe/bower_components/videogular-controls/vg-controls.js',
+          './app/min-safe/bower_components/videogular-buffering/vg-buffering.js'
+        ],
+        dest: './app/min/bower_components.min.js'
+      },
+      app: {
+        options: {
+          // Replace all 'use strict' statements in the code with a single one at the top
+          banner: "'use strict';\n",
+          process: function(src, filepath) {
+            return '// Source: ' + filepath + '\n' +
+              src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+          },
+        },
+        src: [
+          './app/min-safe/app.module.js',
+          './app/min-safe/cliff-talbott/cliff-talbott.module.js',
+          './app/min-safe/cliff-talbott/cliff-talbott.component.js'
+        ],
+        dest: './app/min/app.min.js'
+      }
+    },
     jshint: {
       options: {
         node: true,
@@ -11,46 +70,33 @@ module.exports = function(grunt) {
           'angular': false
         }
       },
-      all: [
+      beforeconcat: [
         'Gruntfile.js',
         './app/app.module.js',
         './app/cliff-talbott/cliff-talbott.component.js',
         './app/cliff-talbott/cliff-talbott.module.js'
-      ]
-    },
-    ngAnnotate: {
-      options: {
-        singleQuotes: true
-      },
-      app: {
-        files: {
-          './app/min-safe/app.module.js': ['./app/app.module.js'],
-          './app/min-safe/cliff-talbott/cliff-talbott.component.js': ['./app/cliff-talbott/cliff-talbott.component.js'],
-          './app/min-safe/cliff-talbott/cliff-talbott.module.js': ['./app/cliff-talbott/cliff-talbott.module.js']
-        }
-      }
-    },
-    concat: {
-      js: {
-        src: ['./app/min-safe/app.module.js', './app/min-safe/cliff-talbott/*.js'],
-        dest: './app/min/app.min.js'
-      }
+      ],
+      afterconcat: ['./app/min/app.min.js']
     },    
     uglify: {
-      js: {
-        src: ['./app/min/app.min.js'],
-        dest: ['./app.min/app.min.js']
+      bower_components: {
+        src: './app/min/bower_components.min.js',
+        dest: './app/min/bower_components.min.js'
+      },
+      app: {
+        src: './app/min/app.min.js',
+        dest: './app/min/app.min.js'
       }
     }
   });
   
   // Load the plugins to provide tasks
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-ng-annotate');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'ngAnnotate', 'concat', 'uglify']);
+  grunt.registerTask('default', ['ngAnnotate', 'concat', 'jshint', 'uglify']);
 };
 
---
+++ app/min-safe/app.module.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Define the `cliffTalbottInterviewApp` module
+angular.module('cliffTalbottInterviewApp', [
+  // ...which depends on the `cliffTalbott` module
+  'cliffTalbott',
+]);
