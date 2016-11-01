b3a0303cc87a6297927c6b6efb50e6563fd4b3c8
Clint Talbott <cctalbott@gmail.com>
2016-11-01 10:23:02 -0500

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      app: {
+        files {
+          './app/min-safe/app.module.js': ['./app/app.module.js'],
+          './app/min-safe/cliff-talbott/cliff-talbott.component.js': ['./app/cliff-talbott/cliff-talbott.component.js'],
+          './app/min-safe/cliff-talbott/cliff-talbott.module.js': ['./app/cliff-talbott/cliff-talbott.module.js']
+        }
+      }
+    },
+    concat: {
+      js: {
+        src: ['./app/min-safe/app.module.js', './app/min-safe/cliff-talbott/*.js'],
+        dest: './app/min/app.min.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugins to provide tasks
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+
+  // Default task(s).
+  grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+
+};
